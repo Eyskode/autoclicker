e6aa1754450b583dd3056cc93dea8c998589916c
Eyskode <36134170+Eyskode@users.noreply.github.com>
2019-07-09 18:05:15 +0200

    Add files via upload

---
+++ autoclicker.py
@@ -0,0 +1,74 @@
+import pyautogui
+
+
+
+
+a = "Autoclicker"
+b = "Credits"
+c = "Exit"
+
+print ("                                     //////////  CREATED BY TKODE  //////////")
+
+
+choices = {a: 'Autoclicker', b: 'Credits', c: 'Exit'}
+
+answer = pyautogui.confirm('Choose one.' , buttons=list(choices))
+
+if answer == a:
+	  
+	  num = pyautogui.prompt("How many clicks you want to do?  ")
+
+	  print ("You selected Autoclicker")
+	  
+	  pyautogui.alert(text=choices[answer], button='OK')
+	  
+	  for num in range (int(num)):
+	  
+	  	pyautogui.click()
+
+
+	  	
+
+	
+		
+
+
+		
+	
+	
+ 
+
+if answer == b:
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("")
+	print("                                 You can follow me on instagram @eyskode.exe")
+	
+	pyautogui.alert(text=choices[answer], button='OK')
+
+
+
+
+if answer == c:
+	
+	print("Exit")
+
+
+
+
+
+
+
+
+ 
+
+
+
